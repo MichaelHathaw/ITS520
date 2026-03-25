57855b89dd6487ce632cc1ee1f468473837dbee3
Michael Hathaway <mhathaw@purdue.edu>
2026-03-25 14:23:11 -0500

    Update generate_samples.py

--- Perceptron_web/generate_samples.py
+++ Perceptron_web/generate_samples.py
@@ -8,40 +8,15 @@ def empty_grid():
 def flatten(grid):
     return [cell for row in grid for cell in row]
 
-def print_grid(grid):
-    for row in grid:
-        print("".join(str(x) for x in row))
-    print()
+def grid_key(grid):
+    return tuple(flatten(grid))
 
-def grid_to_tuple(grid):
-    return tuple(tuple(row) for row in grid)
-
-def make_L(top_row, left_col, height, width):
+def make_T(top_row, left_col, bar_width, stem_height, stem_offset):
     """
-    L shape:
-    - vertical line goes downward from (top_row, left_col)
-    - bottom foot goes right from the bottom of that vertical line
-    """
-    g = empty_grid()
-
-    # vertical part
-    for r in range(top_row, top_row + height):
-        g[r][left_col] = 1
-
-    # bottom horizontal foot
-    bottom_row = top_row + height - 1
-    for c in range(left_col, left_col + width):
-        g[bottom_row][c] = 1
-
-    return g
-
-def make_T(top_row, left_col, bar_width, stem_height, stem_col_offset):
-    """
-    T shape:
-    - top horizontal bar starts at (top_row, left_col) with length bar_width
-    - stem starts on the same top row and goes downward
-    - stem_col_offset says where the stem is under the bar
-      e.g. if left_col=1 and stem_col_offset=2, stem is at col 3
+    T:
+    - top horizontal bar of width 2..4
+    - vertical stem of height 2..4
+    - stem can be under ANY bar cell
     """
     g = empty_grid()
 
@@ -49,109 +24,72 @@ def make_T(top_row, left_col, bar_width, stem_height, stem_col_offset):
     for c in range(left_col, left_col + bar_width):
         g[top_row][c] = 1
 
-    stem_col = left_col + stem_col_offset
-
-    # vertical stem
+    # stem
+    stem_col = left_col + stem_offset
     for r in range(top_row, top_row + stem_height):
         g[r][stem_col] = 1
 
     return g
 
-def generate_all_Ls():
+def make_L(top_row, left_col, height, foot_width):
     """
-    Generate every possible L in a 4x4 grid:
-    - height: 2..4
-    - width: 2..4
-    - placed anywhere it fits
+    L:
+    - vertical line of height 2..4
+    - bottom foot of width 2..4 extending right
     """
-    unique = {}
-    samples = []
+    g = empty_grid()
 
-    for height in range(2, GRID + 1):
-        for width in range(2, GRID + 1):
-            for top_row in range(0, GRID - height + 1):
-                for left_col in range(0, GRID - width + 1):
-                    g = make_L(top_row, left_col, height, width)
-                    key = grid_to_tuple(g)
-                    if key not in unique:
-                        unique[key] = True
-                        samples.append({
-                            "x": flatten(g),
-                            "y": 0,   # L = 0
-                            "shape": "L"
-                        })
-    return samples
+    # vertical line
+    for r in range(top_row, top_row + height):
+        g[r][left_col] = 1
 
-def generate_all_Ts():
-    """
-    Generate every possible T in a 4x4 grid:
-    - bar_width: 2..4
-    - stem_height: 2..4
-    - stem can be at any position under the top bar
-    - placed anywhere it fits
-    """
-    unique = {}
-    samples = []
+    # bottom foot
+    bottom_row = top_row + height - 1
+    for c in range(left_col, left_col + foot_width):
+        g[bottom_row][c] = 1
 
+    return g
+
+def generate_T_samples():
+    samples = {}
     for bar_width in range(2, GRID + 1):
         for stem_height in range(2, GRID + 1):
-            for stem_col_offset in range(bar_width):
+            for stem_offset in range(bar_width):  # stem can be under any bar cell
                 for top_row in range(0, GRID - stem_height + 1):
                     for left_col in range(0, GRID - bar_width + 1):
-                        g = make_T(top_row, left_col, bar_width, stem_height, stem_col_offset)
-                        key = grid_to_tuple(g)
-                        if key not in unique:
-                            unique[key] = True
-                            samples.append({
-                                "x": flatten(g),
-                                "y": 1,   # T = 1
-                                "shape": "T"
-                            })
-    return samples
+                        g = make_T(top_row, left_col, bar_width, stem_height, stem_offset)
+                        samples[grid_key(g)] = {
+                            "x": flatten(g),
+                            "y": 1
+                        }
+    return list(samples.values())
 
-def verify_examples():
-    """
-    Verify the two patterns you mentioned are recognized as Ts.
-    """
-    ex1 = [
-        [0,0,0,0],
-        [0,1,1,1],
-        [0,0,1,0],
-        [0,0,1,0]
-    ]
-
-    ex2 = [
-        [0,0,0,0],
-        [0,0,0,0],
-        [0,1,1,1],
-        [0,0,1,0]
-    ]
-
-    t_samples = generate_all_Ts()
-    t_set = {tuple(s["x"]) for s in t_samples}
-
-    print("Example 1 is T:", tuple(flatten(ex1)) in t_set)
-    print("Example 2 is T:", tuple(flatten(ex2)) in t_set)
+def generate_L_samples():
+    samples = {}
+    for height in range(2, GRID + 1):
+        for foot_width in range(2, GRID + 1):
+            for top_row in range(0, GRID - height + 1):
+                for left_col in range(0, GRID - foot_width + 1):
+                    g = make_L(top_row, left_col, height, foot_width)
+                    samples[grid_key(g)] = {
+                        "x": flatten(g),
+                        "y": 0
+                    }
+    return list(samples.values())
 
 def main():
-    Ls = generate_all_Ls()
-    Ts = generate_all_Ts()
+    t_samples = generate_T_samples()
+    l_samples = generate_L_samples()
+    dataset = t_samples + l_samples
 
-    dataset = Ls + Ts
-
-    print("Total L samples:", len(Ls))
-    print("Total T samples:", len(Ts))
-    print("Total dataset size:", len(dataset))
-    print()
-
-    verify_examples()
+    print("Total T samples:", len(t_samples))
+    print("Total L samples:", len(l_samples))
+    print("Total dataset:", len(dataset))
 
     with open("dataset.json", "w") as f:
         json.dump(dataset, f, indent=2)
 
-    print("\nSaved dataset to dataset.json")
-    print("\nFirst 10 samples:")
-    print(json.dumps(dataset[:10], indent=2))
+    print("Saved dataset.json")
 
 if __name__ == "__main__":
     main()
